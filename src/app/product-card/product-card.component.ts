e9f700906b3bc4a5c2fa1241904f06f7b1be532e
georgreen <georgreen.ngunga@andela.com>
2017-10-08 18:28:26 +0300

    feat(product) implement product catalog
      - Add Product view for users
      - Add sidebar for filtering
      - Add search capabilities
      - Add product card component
      - Add filter product component

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.css']
+})
+export class ProductCardComponent implements OnInit {
+  @Input('product') product;
+  @Input('showActions') showActions = true;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
